6b4a5fc089fe3ce675d9a1063913de25b8eba880
Eloise Brosseau <eloise.brosseau@gmail.com>
2022-05-21 15:58:08 -0400

    Fix domain in updateGroupXScale

--- TP2/src/scripts/viz.js
+++ TP2/src/scripts/viz.js
@@ -7,7 +7,7 @@
  * @param {number} width The width of the graph
  */
 export function updateGroupXScale (scale, data, width) {
-  scale.domain([d3.min(data), d3.max(data)]).range([0, width])
+  scale.domain(d3.extent(data, d => d.Act)).range([0, width])
 }
 
 /**
