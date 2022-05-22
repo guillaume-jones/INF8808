83f87a0e51b7abb2155ec1de8cac71d291cb95a8
Guillaume Jones <guillaume.jones.mint@gmail.com>
2022-05-22 15:18:07 -0400

    Fix comments

--- TP2/src/scripts/tooltip.js
+++ TP2/src/scripts/tooltip.js
@@ -5,20 +5,13 @@
  * @returns {string} The tooltip contents
  */
 export function getContents (d) {
-  /* TODO : Define and return the tooltip contents including :
-      + A title stating the hovered element's group, with:
-        - Font family: Grenze Gotish
-        - Font size: 24px
-        - Font weigth: normal
-      + A bold label for the player name followed
-        by the hovered elements's player's name
-      + A bold label for the player's line count
-        followed by the number of lines
-  */
+  console.log(d)
+  // TODO : Replace with actual d
   d = {
     Player: 'Test',
     Count: 100
   }
+
   return (`<div>
   <p id="tooltip-title">Act 1</p>
   <p class="tooltip-value"><b>Player :</b> ${d.Player}</p>
