3280f4fd55a944e75491c46a3e61c332f00cc1cc
Guillaume Jones <guillaume.jones.mint@gmail.com>
2022-05-22 17:59:21 -0400

    Partially fix tooltip Does not display Act yet

--- TP2/src/scripts/tooltip.js
+++ TP2/src/scripts/tooltip.js
@@ -6,14 +6,8 @@
  */
 export function getContents (d) {
   console.log(d)
-  // TODO : Replace with actual d
-  d = {
-    Player: 'Test',
-    Count: 100
-  }
-
   return (`<div>
-  <p id="tooltip-title">Act 1</p>
+  <p id="tooltip-title">Act ${d.Act}</p>
   <p class="tooltip-value"><b>Player :</b> ${d.Player}</p>
   <p class="tooltip-value"><b>Count :</b> ${d.Count}</p>
   </div>`)
