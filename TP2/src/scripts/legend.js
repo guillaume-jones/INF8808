679142566d2ceec99ce173d6d9de0af9784c39d8
Guillaume Jones <guillaume.jones.mint@gmail.com>
2022-05-22 14:10:57 -0400

    Add legend Added 1 style to style.css for legend text Used 1 SVG + 1 p for each actor

--- TP2/src/scripts/legend.js
+++ TP2/src/scripts/legend.js
@@ -5,7 +5,29 @@
  * @param {*} color The color scale used throughout the visualisation
  */
 export function draw (data, color) {
-  // TODO : Generate the legend in the div with class "legend". Each SVG rectangle
-  // should have a width and height set to 15.
-  // Tip : Append one div per legend element using class "legend-element".
+  const legendElement = d3.select('.legend')
+    .selectAll('div')
+    .data(data)
+    .enter()
+    .append('div')
+    .attr('class', 'legend-element')
+
+  legendElement
+    .append('svg')
+    .attr('width', 15)
+    .attr('height', 15)
+    .append('rect')
+    .attr('x', 0)
+    .attr('y', 0)
+    .attr('width', 15)
+    .attr('height', 15)
+    .attr('fill', color)
+    .attr('stroke', '#000000')
+    .attr('stroke-width', '1')
+
+  legendElement.append('p')
+    .attr('class', 'legend-text')
+    .text((actor) => {
+      return actor
+    })
 }
