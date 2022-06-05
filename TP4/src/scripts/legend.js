bf866bb53c9477ec5058bb04bb0ba36ee427d994
Guillaume Jones <guillaume.jones.mint@gmail.com>
2022-06-05 01:12:00 -0400

    Add legend

--- TP4/src/scripts/legend.js
+++ TP4/src/scripts/legend.js
@@ -8,6 +8,12 @@ import d3Legend from 'd3-svg-legend'
  * @param {number} width The width of the graph, used to place the legend
  */
 export function drawLegend (colorScale, g, width) {
-  // TODO : Draw the legend using d3Legend
-  // For help, see : https://d3-legend.susielu.com/
+  const legend = d3Legend.legendColor()
+    .scale(colorScale)
+    .shape('circle')
+    .title('Legend')
+
+  g.append('g')
+    .attr('transform', 'translate(' + width + ', -20)')
+    .call(legend)
 }
