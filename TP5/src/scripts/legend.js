fc6fac46a8877bd6e80b7e235c4b2690b4f28914
Guillaume Jones <guillaume.jones.mint@gmail.com>
2022-06-07 23:23:49 -0400

    Finish legend

--- TP5/src/scripts/legend.js
+++ TP5/src/scripts/legend.js
@@ -7,6 +7,12 @@ import d3Legend from 'd3-svg-legend'
  * @param {*} g The d3 Selection of the SVG g elemnt containing the legend
  */
 export function drawLegend (colorScale, g) {
-  // TODO : Generate the legend
-  // For help, see : https://d3-legend.susielu.com/
+  const legend = d3Legend.legendColor()
+    .scale(colorScale)
+    .shape('circle')
+    .title('Légende')
+
+  g.append('g')
+    .attr('transform', 'translate(50, 125)')
+    .call(legend)
 }
