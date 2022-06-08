a41aa27752ac76e0495ab01473e7d1329752567e
Guillaume Jones <guillaume.jones.mint@gmail.com>
2022-06-07 23:05:51 -0400

    Finish map markers

--- TP5/src/scripts/viz.js
+++ TP5/src/scripts/viz.js
@@ -25,7 +25,7 @@ export function mapBackground (data, path, showMapLabel) {
     .attr('d', path)
     .attr('fill', '#ced1c5')
     .attr('stroke', '#ffffff')
-    .attr('stroke-width', 1.5)
+    .attr('stroke-width', 1)
     .on('mouseover', (d) => showMapLabel(d, path))
     .on('mouseout', () => d3.select('.hover').remove())
 }
@@ -54,12 +54,27 @@ export function showMapLabel (d, path) {
  * Displays the markers for each street on the map.
  *
  * @param {object[]} data The street data to be displayed
- * @param {*} color The color scaled used to determine the color of the circles
+ * @param {*} color The color scale used to determine the color of the circles
  * @param {*} panel The display panel, which should be dislayed when a circle is clicked
  */
 export function mapMarkers (data, color, panel) {
-  // TODO : Display the map markers.
-  // Their color corresponds to the type of site and their outline is white.
-  // Their radius is 5 and goes up to 6 while hovered by the cursor.
-  // When clicked, the panel is displayed.
+  d3.select('.main-svg')
+    .selectAll('circle')
+    .data(data.features)
+    .enter()
+    .append('circle')
+    .attr('class', 'marker')
+    .attr('r', 5)
+    .attr('cx', (d) => d.x)
+    .attr('cy', (d) => d.y)
+    .attr('fill', (d) => color(d.properties.TYPE_SITE_INTERVENTION))
+    .attr('stroke', '#ffffff')
+    .attr('stroke-width', 1)
+    .on('mouseover', function () {
+      d3.select(this).attr('r', 6)
+    })
+    .on('mouseout', function () {
+      d3.select(this).attr('r', 5)
+    })
+    .on('click', (d) => panel.display(d, color))
 }
