38b319d39d50599928e33d4929c56dcc98bb9b7c
Eloise Brosseau <eloise.brosseau@gmail.com>
2022-06-22 21:58:24 -0400

    Add index files

---
+++ Projet/src/index.js
@@ -0,0 +1,16 @@
+// 'use strict'
+
+/**
+ * @file This file is the entry-point for the the code for TP5 for the course INF8808.
+ * @author Éloïse Brosseau
+ * @author Guillaume Jones
+ * @author Charles-Olivier Loiselle
+ * @version v1.0.0
+ */
+
+(function (d3) {
+  const svgSize = {
+    width: 800,
+    height: 625
+  }
+})(d3)
