ed1f0376595f1584a4d5715f02c086c209cdbb0c
Charles-Olivier Loiselle <charles.olivier.loiselle@gmail.com>
2022-06-23 21:06:57 -0400

    Preprocess

---
+++ Projet/src/scripts/preprocess.js
@@ -0,0 +1,11 @@
+/**
+ * Generates the data for the map:
+ *  - Filters data for the selected year
+ *  - Computes the sum of each counter for the given year
+ *  - Extracts the x and y position of each counter
+ *  - Determines the Neighborhood of each counter based on coordinates
+ *  - Extracts the name of the counter
+ * 
+ * Returns a list containing all the information for each counter on the given year
+ */
+
