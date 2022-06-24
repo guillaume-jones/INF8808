d629fec290c55a1fe354b9f6502822754cd7332a
Charles-Olivier Loiselle <charles.olivier.loiselle@gmail.com>
2022-06-24 03:12:23 -0400

    Bouton qui marche pas

---
+++ Projet/src/scripts/year_button.js
@@ -0,0 +1,48 @@
+/**
+ * Draws the button leading to a dropdown menu to select the desired year
+ * 
+ * @param {*} g The d3 Selection of the graph's g SVG element
+ * @param {number} year The year to display
+ * @param {number} width The width of the graph, used to place the button
+ */
+ export function drawButton (g, year, width) {
+    const button = g.append('g')
+      .attr('class', 'button')
+      .attr('transform', 'translate(' + width + ', 140)')
+      .attr('width', 130)
+      .attr('height', 25)
+  
+    button.append('rect')
+      .attr('width', 130)
+      .attr('height', 30)
+      .attr('fill', '#f4f6f4')
+      .on('mouseenter', function () {
+        d3.select(this).attr('stroke', '#362023')
+      })
+      .on('mouseleave', function () {
+        d3.select(this).attr('stroke', '#f4f6f4')
+      })
+  
+    button.append('text')
+      .attr('x', 65)
+      .attr('y', 15)
+      .attr('text-anchor', 'middle')
+      .attr('dominant-baseline', 'middle')
+      .attr('class', 'button-text')
+      .text('Data for year ' + year)
+      .attr('font-size', '10px')
+      .attr('fill', '#362023')
+  }
+
+  /**
+   * Handles the selection of the year by the user by clicking
+   */
+  export function yearSelection () {
+    d3.select('.button')
+      .on('click', () => {
+        const previousYear = currentYear
+        currentYear = (currentYear === 2009 ? 2022 : 2009)
+        build() // Update quand on va avoir la carte
+        d3.select('.button').select('.button-text').text('Data for year ' + previousYear)
+      })
+  }
