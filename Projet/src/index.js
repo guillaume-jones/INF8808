c17c53280ec958ce35361e4fcf546710edb9cc21
Eloise Brosseau <eloise.brosseau@gmail.com>
2022-06-24 12:32:16 -0400

    Fix Montreal map display

--- Projet/src/index.js
+++ Projet/src/index.js
@@ -15,13 +15,16 @@ import { getMontrealData, getProjection, getPath } from './scripts/geography';
     height: 625,
   };
 
-  const montreal = getMontrealData();
+  const montreal = await getMontrealData();
   const projection = getProjection();
   const path = getPath(projection);
 
-  mapViz.mapBackground(montreal, path);
   mapViz.setCanvasSize(svgSize.width, svgSize.height);
   mapViz.generateMapG(svgSize.width, svgSize.height);
+
+  d3.json('./montreal.json').then(function (data) {
+    mapViz.mapBackground(data, path);
+  });
 
   const dataset = createDataset();
   console.log(dataset);
@@ -29,6 +32,6 @@ import { getMontrealData, getProjection, getPath } from './scripts/geography';
   const lineChartData = createLineChartData(dataset, montreal);
   console.log(lineChartData);
 
-  year_button.drawButton('#graph-g', 2009, svgSize.width);
+  // year_button.drawButton('#graph-g', 2009, svgSize.width);
   year_button.yearSelection(2009);
 })(d3);
