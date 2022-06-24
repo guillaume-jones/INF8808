ffc70278fc43853fe2cbdf0e842d82b749469be7
Eloise Brosseau <eloise.brosseau@gmail.com>
2022-06-23 23:51:53 -0400

    Fix filterData in index.js

--- Projet/src/index.js
+++ Projet/src/index.js
@@ -4,9 +4,9 @@ import 'regenerator-runtime/runtime.js'
 (async function (d3) {
   const years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
 
-  years.forEach(year => {
-    d3.csv('./comptage_velo_' + year + '.csv', (bikeData) => {
-      d3.csv('./localisation_des_compteurs_velo.csv', (counterData) => {
+  d3.csv('./localisation_des_compteurs_velo.csv', (counterData) => {
+    years.forEach(year => {
+      d3.csv('./comptage_velo_' + year + '.csv', (bikeData) => {
         filterData(bikeData, counterData)
       })
     })
