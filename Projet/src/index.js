5fc3df8230dd6fe3c720cd71a6bcc784b86f35fd
Eloise Brosseau <eloise.brosseau@gmail.com>
2022-06-23 23:47:54 -0400

    Remove unnecessary semicolon

--- Projet/src/index.js
+++ Projet/src/index.js
@@ -1,13 +1,8 @@
-import { filterData } from './scripts/preprocess';
-import 'regenerator-runtime/runtime.js';
+import { filterData } from './scripts/preprocess'
+import 'regenerator-runtime/runtime.js'
 
 (async function (d3) {
-  const svgSize = {
-    width: 800,
-    height: 625
-  }
-
-  const years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021];
+  const years = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
 
   years.forEach(year => {
     d3.csv('./comptage_velo_' + year + '.csv', (bikeData) => {
