7162fb0d4e9ca1463c26e0899aa9b815bedf8cd3
Eloise Brosseau <eloise.brosseau@gmail.com>
2022-06-24 02:18:26 -0400

    Add mapViz.js

--- Projet/src/index.js
+++ Projet/src/index.js
@@ -1,5 +1,6 @@
 import { filterData } from './scripts/preprocess';
 import 'regenerator-runtime/runtime.js';
+import * as mapViz from './scripts/mapViz';
 
 import {
   getMontrealData,
@@ -14,9 +15,13 @@ import {
     height: 625,
   };
 
-  const montreal = await getMontrealData();
+  const montreal = getMontrealData();
   const projection = getProjection();
   const path = getPath(projection);
+
+  mapViz.mapBackground(montreal, path);
+  mapViz.setCanvasSize(svgSize.width, svgSize.height);
+  mapViz.generateMapG(svgSize.width, svgSize.height);
 
   const years = [
     2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020,
@@ -24,7 +29,7 @@ import {
   ];
 
   await d3.csv('./localisation_des_compteurs_velo.csv', (counterData) => {
-    years.forEach(year => {
+    years.forEach((year) => {
       d3.csv('./comptage_velo_' + year + '.csv', (bikeData) => {
         filterData(bikeData, counterData);
       });
